138d757ab91aae67647fb6be51e8aa47066204c7
LinQ0xAF <sbr07050@gmail.com>
2025-11-11 14:49:19 +0900

    integrating UI Panel logic

--- Assets/Scripts/JeongGyo/UI/UIControllPanel.cs
+++ Assets/Scripts/JeongGyo/UI/UIControllPanel.cs
@@ -9,27 +9,41 @@ public class UIControllPanel : MonoBehaviour
 {
     [SerializeField]
     private GameObject SingeleUIPanelObj;
-    
+
     [SerializeField]
     private GameObject MultiUIPanelObj;
+    
+    [SerializeField]
+    private GameObject _MainUIPanelObj;
 
     [SerializeField]
     private bool isSingleUI = true;
 
-    
+    private int _DartsLayerIndex;
+
+    void Awake()
+    {
+        _DartsLayerIndex = LayerMask.NameToLayer("Darts");
+    }
+
+
     void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Dart"))
+        if (collision.gameObject.layer == _DartsLayerIndex)
         {
+            Debug.Log("Dart hit UI Control Panel");
+
             if (isSingleUI)
             {
                 SingeleUIPanelObj.SetActive(true);
                 MultiUIPanelObj.SetActive(false);
+                _MainUIPanelObj.SetActive(false);
             }
             else
             {
                 SingeleUIPanelObj.SetActive(false);
                 MultiUIPanelObj.SetActive(true);
+                _MainUIPanelObj.SetActive(false);
             }
             
         }
